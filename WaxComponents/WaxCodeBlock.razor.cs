bc45feb2b6a452332fc4b9d288bf980b1dd26976
lllggghhhaaa <lgha1423@outlook.com>
2021-12-10 19:46:33 -0300

    Code block added

---
+++ WaxComponents/WaxCodeBlock.razor.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace WaxComponents;
+
+public partial class WaxCodeBlock
+{
+    [Parameter]
+    public string Code { get; set; } = String.Empty;
+    
+    [Parameter]
+    public string Language { get; set; }
+
+    private string _lines
+    {
+        get
+        {
+            List<string> lines = new List<string>();
+
+            foreach (int line in Enumerable.Range(1, Code.Split("\n").Length))
+                lines.Add(line.ToString().PadLeft(Code.Split("\n").Length.ToString().Length - line.ToString().Length, ' '));
+
+            return string.Join("\n", lines);
+        }
+    }
+}
