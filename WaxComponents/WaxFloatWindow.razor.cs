8b31f3d3773a7bf2e5cfc74cae5a6da8f371b999
lllggghhhaaa <lgha1423@outlook.com>
2022-11-26 17:28:02 -0300

    FloatWindow added

---
+++ WaxComponents/WaxFloatWindow.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WaxComponents;
+
+public partial class WaxFloatWindow
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string Style { get; set; } = String.Empty;
+
+    public bool Active;
+
+    public void Show()
+    {
+        Active = true;
+        StateHasChanged();
+    }
+
+    public void Hide()
+    {
+        Active = false;
+        StateHasChanged();
+    }
+}
