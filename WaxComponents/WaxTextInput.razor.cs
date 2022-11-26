9507f92fb377e14fe708f5206b78d987eb4a5857
lllggghhhaaa <lgha1423@outlook.com>
2022-11-26 13:42:27 -0300

    Input types added (Text, Password, Email, Number, Tel, Time, Url)

--- WaxComponents/WaxTextInput.razor.cs
+++ WaxComponents/WaxTextInput.razor.cs
@@ -4,17 +4,27 @@ namespace WaxComponents;
 
 public partial class WaxTextInput
 {
-    [Parameter] 
-    public string Value { get; set; } = String.Empty;
-    [Parameter]
-    public string Placeholder { get; set; } = String.Empty;
+    [Parameter] public string Value { get; set; } = String.Empty;
+    [Parameter] public InputType TextType { get; set; } = InputType.Text;
+    [Parameter] public string Placeholder { get; set; } = String.Empty;
+    [Parameter] public string Pattern { get; set; } = String.Empty;
     [Parameter] public string Style { get; set; } = String.Empty;
-    [Parameter]
-    public EventCallback<string> ValueChanged { get; set; }
+    [Parameter] public EventCallback<string> ValueChanged { get; set; }
 
     private async void OnChange(ChangeEventArgs args)
     {
         Value = args.Value?.ToString() ?? String.Empty;
         await ValueChanged.InvokeAsync(Value);
+    }
+
+    public enum InputType
+    {
+        Text,
+        Password,
+        Email,
+        Number,
+        Tel,
+        Time,
+        Url
     }
 }
