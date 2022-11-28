8dfcdd06ad4469894ffc808dadeebf177f0970bf
lllggghhhaaa <lgha1423@outlook.com>
2022-11-28 11:34:58 -0300

    Tabs design changes

---
+++ WaxComponents/WaxTab.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WaxComponents;
+
+public partial class WaxTab
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string Style { get; set; } = String.Empty;
+
+    private List<WaxTabItem> _tabs = new();
+
+    private WaxTabItem? _activeTab;
+
+    public void RegisterTab(WaxTabItem tab)
+    {
+        _tabs.Add(tab);
+
+        tab.SetAsLast();
+        if (_tabs.Count == 1)
+        {
+            tab.SetAsFirst();
+            SetTab(tab);
+        }
+        else
+            _tabs[^2].SetAsLast(true);
+    }
+
+    public void SetTab(WaxTabItem tab)
+    {
+        _activeTab?.ChangeActive(false);
+        tab.ChangeActive(true);
+        
+        _activeTab = tab;
+        StateHasChanged();
+    }
+}
---
+++ WaxComponents/WaxTabItem.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WaxComponents;
+
+public partial class WaxTabItem
+{
+    [Parameter]
+    public RenderFragment? Text { get; set; }
+
+    [Parameter]
+    public RenderFragment? Icon { get; set; }
+
+    [Parameter]
+    public RenderFragment? Content { get; set; }
+
+    [CascadingParameter]
+    public WaxTab? Parent { get; set; }
+    
+    [Parameter]
+    public string FontSize { get; set; } = "12px";
+
+    [Parameter]
+    public string Style { get; set; } = String.Empty;
+
+    private string _bgColor = "white";
+    private string _borderTopRight = "0";
+    private string _borderTopLeft = "0";
+
+    protected override void OnInitialized()
+    {
+        if (Parent is not null) Parent.RegisterTab(this);
+
+        base.OnInitialized();
+    }
+
+    public void ChangeActive(bool active) => _bgColor = active ? "rgba(255, 20, 147, 0.2) !important" : "white";
+
+    public void SetAsFirst(bool remove = false) => _borderTopLeft = remove ? "0" : "5px";
+    public void SetAsLast(bool remove = false) => _borderTopRight = remove ? "0" : "5px";
+
+    private void OnTabClicked()
+    {
+        if (Parent is not null) Parent.SetTab(this);
+    }
+}
