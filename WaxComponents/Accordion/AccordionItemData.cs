49fe345211270f297b9ebb8f4bf674c3c19793dc
lllggghhhaaa <lgha1423@outlook.com>
2026-07-01 01:05:49 -0300

    New design and accordion added

---
+++ WaxComponents/Accordion/AccordionItemData.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WaxComponents.Accordion;
+
+public class AccordionItemData
+{
+    public string Title { get; set; } = string.Empty;
+    public MarkupString Body { get; set; }
+    public RenderFragment? Icon { get; set; }
+}
